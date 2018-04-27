86be85ce5209bda9a6216326123f4f3b98728794
Yuexiang Gao <yxgao@thoughtworks.com>
2018-04-28 00:47:41 +0800

    add a project to run stub as a server by spring code

---
+++ by_code/src/main/java/com/github/kbyyd24/lddstubrunner/LddStubRunnerApplication.java
@@ -0,0 +1,12 @@
+package com.github.kbyyd24.lddstubrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LddStubRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LddStubRunnerApplication.class, args);
+	}
+}
