9f2359644070ca88fc4474a1e08db1826180be5d
Yuexiang Gao <yxgao@thoughtworks.com>
2018-04-28 11:06:34 +0800

    reset by code

--- by_code/src/main/java/com/github/kbyyd24/lddstubrunner/LddStubRunnerApplication.java
+++ by_code/src/main/java/com/github/kbyyd24/lddstubrunner/LddStubRunnerApplication.java
@@ -2,11 +2,13 @@ package com.github.kbyyd24.lddstubrunner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.contract.stubrunner.server.EnableStubRunnerServer;
 
 @SpringBootApplication
+@EnableStubRunnerServer
 public class LddStubRunnerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LddStubRunnerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LddStubRunnerApplication.class, args);
+    }
 }
